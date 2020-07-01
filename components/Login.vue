f10460cea38423254729f573e0956db5a6ccfc8d
Rostyslav Khomitskyu <roshom68@gmail.com>
2020-07-01 04:10:25 +0300

    first commit

---
+++ components/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <span class="loginTitle">Login</span>
+    <input class="loginInput" placeholder="Email"/>
+    <input class="loginInput" placeholder="Password"/>
+  </div>
+</template>
+
+<style>
+.loginInput{
+    display: block;
+    width: 100%;
+}
+.loginTitle{
+    text-transform: uppercase;
+    }
+</style>
+
